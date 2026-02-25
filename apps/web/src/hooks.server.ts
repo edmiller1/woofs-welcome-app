fcfb9e0904eb5dd47c5a20527b987ab3603f8016
Edward Miller <edmiller.me@gmail.com>
2026-02-25 21:07:12 +1300

    removed sentry for now

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,12 +1,3 @@
 import { sequence } from "@sveltejs/kit/hooks";
-import * as Sentry from "@sentry/sveltekit";
 
-Sentry.init({
-  dsn: process.env.PUBLIC_SENTRY_DSN,
-  environment: process.env.PUBLIC_NODE_ENV || "development",
-  tracesSampleRate: 1.0,
-});
-
-export const handleError = Sentry.handleErrorWithSentry();
-
-export const handle = sequence(Sentry.sentryHandle());
+export const handle = sequence();
